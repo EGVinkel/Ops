d94c478c3d6830e8fa3471f186942b5e6d506427
UltimateDadBod <emilvinkel@gmail.com>
2019-10-02 09:14:37 +0200

    Energy counter

---
+++ src/main/webapp/precache-manifest.7a6b7d7f2ffbb212729ce75a28fbf83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a980b1bab94c8342f4edbbbdc45857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54f2df34fd8678cbda94",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "74dd5e3690e12bc64dbe",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "54f2df34fd8678cbda94",
+    "url": "/static/js/2.7d1951f2.chunk.js"
+  },
+  {
+    "revision": "74dd5e3690e12bc64dbe",
+    "url": "/static/js/main.d615a5c1.chunk.js"
+  },
+  {
+    "revision": "a47a7dc11bc622cedb40",
+    "url": "/static/js/runtime~main.cb2ef8e0.js"
+  }
+]);
